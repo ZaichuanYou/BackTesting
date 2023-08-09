1a3202143cb34abd0d7ca0b0264cce0f0cc7086e
Zaichuan You <zxy456@case.edu>
2023-08-09 10:40:58 +0800

    Added high freq data calculation

--- analyzeReturnFluxRate.py
+++ analyzeReturnFluxRate.py
@@ -9,7 +9,7 @@ from cringe import extend_data
 
 follow_interval = 5
 adv_moment_num = 10
-session_length = 236
+session_length = 59
 day_avg = 5
 weighted_avg = False
 
@@ -42,17 +42,59 @@ def analyze_index(data, s_dir, d_dir, cols):
     while i < len(df):
         
         df_session = df.iloc[index]
-
         vol_mean = np.mean(df_session['volume'])
         vol_std = np.std(df_session['volume'])
         df_release = df_session[df_session['volume']>vol_mean+vol_std].index
         ret = df.iloc[df_release]['close'].values-df.iloc[df_release-1]['close'].values
         ret_mean = np.mean(ret)
         Factor = sqrt(np.sum((ret-ret_mean)**2/len(df_release)))
-        df_result = pd.concat([df_result, df.loc[[i]]], ignore_index=True)
+        df_result = pd.concat([df_result, df.loc[[i-241]]], ignore_index=True)
         df_result.iat[-1, 8] = Factor
         df_result.iat[-1, 9] = Factor
         i=i+241
+        index = index+241
+
+    df_result.to_csv(d_dir+'/{}'.format(data)) 
+
+
+
+def analyze_index_HighFreq(data, s_dir, d_dir, cols):
+    """
+        This will calculate the Amount follow index and store the result at a new column.\n
+        This is a modified version which will devide data into high frequency sessions.
+
+        params:
+            data: directory of current data
+            window: look back window of the index calculation
+            s_dir: source directory of the data
+            d_dir: destination directory of the processed data
+            cols: columns that will be keeped in the processed data
+            weight_df: weight of weighted average
+            weight_mean: mean of weight
+    """
+    df = pd.read_csv(s_dir+'/{}'.format(data), index_col=0, engine='pyarrow')
+    df_result = pd.DataFrame(columns=cols)
+
+    df = df.dropna()
+
+    index = np.array(list(range(0,59)))
+
+    for i in range(0, len(df), 241):
+        for n in range(0, int(236/session_length)):
+            df_session = df.iloc[index+n*59]
+            vol_mean = np.mean(df_session['volume'])
+            vol_std = np.std(df_session['volume'])
+            df_release = df_session[df_session['volume']>vol_mean+vol_std].index
+            ret = df.iloc[df_release]['close'].values-df.iloc[df_release-1]['close'].values
+            if len(ret)==0:
+                df_result = pd.concat([df_result, df.loc[[index[0]+n*59]]], ignore_index=True)
+                df_result.iat[-1, 8] = 0
+                df_result.iat[-1, 9] = 0
+            ret_mean = np.mean(ret)
+            Factor = sqrt(np.sum((ret-ret_mean)**2/len(df_session)))
+            df_result = pd.concat([df_result, df.loc[[index[0]+n*59]]], ignore_index=True)
+            df_result.iat[-1, 8] = Factor
+            df_result.iat[-1, 9] = Factor
         index = index+241
 
     df_result.to_csv(d_dir+'/{}'.format(data)) 
@@ -111,8 +153,8 @@ def analyze_Reverse_Imp(data, s_dir, d_dir, cols):
 
 if __name__ == '__main__':
     s_dir = 'C:/Users/21995/Desktop/量化投资/可转债数据/full_data'
-    d_dir = 'C:/Users/21995/Desktop/量化投资/中金/Data/CB_Data_Flux5'
-    cols = ['SecurityID', 'time', 'open', 'high', 'low', 'close', 'volume', 'amount', 'factor', 'index']
+    d_dir = 'C:/Users/21995/Desktop/量化投资/中金/Data/CB_Data_FluxHighFreq'
+    cols = ['time', 'SecurityID', 'open', 'high', 'low', 'close', 'volume', 'amount', 'factor', 'index']
     files = os.listdir(s_dir)
     if not os.path.isdir(d_dir):
         os.makedirs(d_dir)
@@ -122,7 +164,7 @@ if __name__ == '__main__':
         # if file in finishd:
         #     continue
         tic = time.perf_counter()
-        analyze_index(file, s_dir=s_dir, d_dir=d_dir, cols=cols)
+        analyze_index_HighFreq(file, s_dir=s_dir, d_dir=d_dir, cols=cols)
         toc = time.perf_counter()
         print("\r", end="")
         print(f"Processing Data: {int(ind+1)*100//len(files)}%, time taken last file: {toc - tic:0.4f}s, last file: {file}")
